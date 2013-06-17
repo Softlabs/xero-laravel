1dd5572b3800f74a5b36da747206caeaffbda2bb
Istvan Kadar <istvan.kadar@softlabs.co.uk>
2013-06-17 12:27:05 +0200

    Fix register function

--- src/Softlabs/XeroLaravel/XeroLaravelServiceProvider.php
+++ src/Softlabs/XeroLaravel/XeroLaravelServiceProvider.php
@@ -1,36 +1,39 @@
 <?php namespace Softlabs\XeroLaravel;
 
 use Illuminate\Support\ServiceProvider;
-use Xero;
 
 class XeroLaravelServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register($config, $format = 'json')
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
     {
-        return new Xero($config->consumer_key(), $config->consumer_secret(), $config->cert_path_public(),
-            $config->cert_path_private(), $format);
+        $this->app['xero'] = $this->app->share(function($app)
+        {
+            \ClassLoader::addDirectories(array(__DIR__ . '/PHP-Xero'));
+            $config = $this->app['config']['xero'];
+            return new \Xero($config['key'], $config['secret'], $config['publicPath'], $config['privatePath'], $config['format']);
+        });
     }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('xero');
+    }
 
 }
